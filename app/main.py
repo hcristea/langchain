1d955f8731cafa5d6262f3132b437ee5452803df
Horatiu Cristea <horatiu.cristea@gmail.com>
2024-04-11 16:49:28 +0300

    Add optional arguments

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 import typer
 
 
-def main(name: str):
-    print(f"Hello {name}!")
+app = typer.Typer()
+
+
+@app.command()
+def hello(name: str, lastname: str = ""):
+    print(f"Hello {name} {lastname}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
