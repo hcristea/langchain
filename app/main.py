6e55132189ddf88863dcbbb52dbe9a1ce43522f9
Horatiu Cristea <horatiu.cristea@gmail.com>
2024-04-12 16:45:19 +0300

    Add the quickstart langchain example

--- app/main.py
+++ app/main.py
@@ -1,20 +1,46 @@
 import typer
-
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from rich.progress import Progress, SpinnerColumn, TextColumn
+from utils import print_md
 
 app = typer.Typer()
 
+load_dotenv()
+
 
 @app.command()
+def start():
+    llm = ChatOpenAI()
+    result = llm.invoke("how can langsmith help with testing?")
+    print(result)
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are world class technical documentation writer. Answer with a markdown."),
+        ("user", "{input}")
+    ])
+    output_parser = StrOutputParser()
+
+    chain = prompt | llm | output_parser
+
+    with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            transient=True,
+    ) as progress:
+        progress.add_task(description="Processing...", total=None)
+        result = chain.invoke({"input": "how can langsmith help with testing?"})
+    # print(result)
+
+    print_md(result)
+
+
+
+@ app.command()
 def hello(name: str, lastname: str = ""):
     print(f"Hello {name} {lastname}")
-
-
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
 
 
 if __name__ == "__main__":
