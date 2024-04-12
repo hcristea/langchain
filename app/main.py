89b254f166dfef3c0a546da4cdaf66104bec8e9a
Horatiu Cristea <horatiu.cristea@gmail.com>
2024-04-12 17:06:46 +0300

    Format code

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from rich.progress import Progress, SpinnerColumn, TextColumn
-from utils import print_md
 
 app = typer.Typer()
 
@@ -14,31 +13,30 @@ load_dotenv()
 @app.command()
 def start():
     llm = ChatOpenAI()
-    result = llm.invoke("how can langsmith help with testing?")
-    print(result)
+    # result = llm.invoke("how can langsmith help with testing?")
+    # print(result)
 
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are world class technical documentation writer. Answer with a markdown."),
-        ("user", "{input}")
-    ])
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are world class technical documentation writer."),
+            ("user", "{input}"),
+        ]
+    )
     output_parser = StrOutputParser()
 
     chain = prompt | llm | output_parser
 
     with Progress(
-            SpinnerColumn(),
-            TextColumn("[progress.description]{task.description}"),
-            transient=True,
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
     ) as progress:
-        progress.add_task(description="Processing...", total=None)
+        progress.add_task(description="AI thinking...", total=None)
         result = chain.invoke({"input": "how can langsmith help with testing?"})
-    # print(result)
-
-    print_md(result)
+    print(result)
 
 
-
-@ app.command()
+@app.command()
 def hello(name: str, lastname: str = ""):
     print(f"Hello {name} {lastname}")
 
