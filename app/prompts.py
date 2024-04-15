3c2e681fce7863e51c31afe38753f22a8f34a1c0
Horatiu Cristea <horatiu.cristea@gmail.com>
2024-04-15 17:52:48 +0300

    Add examples

--- app/prompts.py
+++ app/prompts.py
@@ -1,7 +1,10 @@
 import typer
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import FewShotPromptTemplate, PromptTemplate
 from langchain_openai import ChatOpenAI
+from rich.progress import Progress, SpinnerColumn, TextColumn
+
+from utils import BUNDY_WISDOM, invoke_w_spinner, loading_message
 
 load_dotenv()
 
@@ -12,18 +15,18 @@ app = typer.Typer()
 def basic():
     llm = ChatOpenAI(model="gpt-3.5-turbo")
     prompt = """You are a travel agent that helps with people's trips on a budget. 
-    
+
 Question: What would be a good itinerary for spending three days in Romania and 3 days in Israel?
 
 Answer: """
 
-    result = llm.invoke(prompt)
-    print(result.context)
+    result = invoke_w_spinner(llm, prompt)
+    print(result.content)
 
 
 @app.command()
 def template():
-    llm = ChatOpenAI(model="gpt-4-turbo")
+    llm = ChatOpenAI(model="gpt-3.5-turbo")
 
     template = """You are a travel agent that helps with people's trips about {interest} on a budget of {budget}. 
     
@@ -32,11 +35,79 @@ Question: What would be a good itinerary for spending three days in Romania and 
 Answer: """
 
     prompt_template = PromptTemplate(
-        input_variables=["interest", "budget"],
-        template=template
+        input_variables=["interest", "budget"], template=template
     )
     prompt = prompt_template.format(interest="fishing", budget="5000 USD")
-    result = llm.invoke(prompt)
+
+    result = invoke_w_spinner(llm, prompt)
+    print(result.content)
+
+
+@app.command()
+def examples():
+    llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+    prompt = """The following are excerpts from conversations with an AI
+assistant. The assistant is typically sarcastic and witty, producing
+creative  and funny responses to the users questions. Here are some
+examples: 
+
+User: How are you?
+AI: I can't complain but sometimes I still do.
+
+User: What time is it?
+AI: It's time to get a watch.
+
+User: What is the meaning of life?
+AI: """
+
+    result = invoke_w_spinner(llm, prompt)
+    print(result.content)
+
+
+@app.command()
+def few_shot():
+    llm = ChatOpenAI(model="gpt-3.5-turbo")
+    llm.temperature = 0.1
+
+    # create a example template
+    example_template = """
+    User: {question}
+    AI: {answer}
+    """
+
+    # create a prompt example from above template
+    example_prompt = PromptTemplate(
+        input_variables=["question", "answer"], template=example_template
+    )
+
+    # now break our previous prompt into a prefix and suffix
+    # the prefix is our instructions
+    prefix = """The following are excerpts from conversations with an AI
+assistant. The assistant is typically sarcastic and witty, producing
+creative  and funny responses to the users questions. Here are some
+examples: 
+"""
+    # and the suffix our user input and output indicator
+    suffix = """
+User: {question}
+AI: """
+
+    # now create the few shot prompt template
+    few_shot_prompt_template = FewShotPromptTemplate(
+        examples=BUNDY_WISDOM,
+        example_prompt=example_prompt,
+        prefix=prefix,
+        suffix=suffix,
+        input_variables=["question"],
+        example_separator="\n\n",
+    )
+
+    question = "What is the meaning of life?"
+    prompt = few_shot_prompt_template.format(question=question)
+    print(prompt)
+
+    result = invoke_w_spinner(llm, prompt)
     print(result.content)
 
 
--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,7 @@ import random
 
 from rich.console import Console
 from rich.markdown import Markdown
+from rich.progress import Progress, SpinnerColumn, TextColumn
 
 
 def print_md(markdown: str):
@@ -29,5 +30,72 @@ def loading_message():
     return random.choice(messages)
 
 
-def count_tokens(chain, query):
-    pass
+def invoke_w_spinner(llm, query, print_cost: bool = True):
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description] {task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description=loading_message(), total=None)
+        result = llm.invoke(query)
+    if print_cost:
+        token_usage = result.response_metadata.get("token_usage")
+        print(
+            "Spent a total of {total_tokens} tokens. "
+            "(completion: {completion_tokens}, prompt: {prompt_tokens})".format(**token_usage)
+        )
+    return result
+
+
+BUNDY_WISDOM = [
+    {
+        "question": "What's the secret to a happy marriage?",
+        "answer": "Happy marriage? That's like asking for a unicorn that does your taxes. But if you want my advice, "
+        "it's a good TV and a lock on the bathroom door.",
+    },
+    {
+        "question": "How do you handle stress?",
+        "answer": "Stress? I have four words for you: beer, bowling, and avoiding the family. Oh wait, that's more "
+        "than four. See? Math is stressful.",
+    },
+    {
+        "question": "What's the best way to save money?",
+        "answer": "Easy, don't get married and don't have kids. Or, if you've already made those mistakes, "
+        "just don't leave the couch. Works for me.",
+    },
+    {
+        "question": "What do you think about modern fashion?",
+        "answer": "Modern fashion? Ha! It's like they took the ugliest stuff from my high school years and decided to "
+        "bring it back. Give me a good old-fashioned flannel and jeans any day.",
+    },
+    {
+        "question": "How do you feel about parenting?",
+        "answer": "Parenting? You mean that thing I do when I'm not watching TV or avoiding Peg's 'romantic' "
+        "advances? It's like being a referee in a never-ending game where everyone loses.",
+    },
+    {
+        "question": "What's the best way to spend a weekend?",
+        "answer": "The best way to spend a weekend? Simple: me, a six-pack, and a 'Married... with Children' "
+        "marathon. Ah, the classics never get old.",
+    },
+    {
+        "question": "What's your opinion on modern technology?",
+        "answer": "Modern technology? If it doesn't help me avoid my family or make my beer colder, "
+        "I'm not interested. But hey, at least I can change the TV channels without leaving the couch.",
+    },
+    {
+        "question": "What's your favorite pastime?",
+        "answer": "Favorite pastime? Besides dreaming of a life without Peg and the kids? I'd say bowling. It's the "
+        "only place where I'm a king and my throne is a rented pair of shoes.",
+    },
+    {
+        "question": "What's your advice for a successful career?",
+        "answer": "Successful career? That ship sailed when I said 'I do' to Peg. But if you're lucky enough to still "
+        "have a shot, avoid anything that requires pants and a tie. Trust me.",
+    },
+    {
+        "question": "What's the key to happiness?",
+        "answer": "Happiness? That's easy. Lower your expectations, keep a full beer fridge, and never, ever run out "
+        "of toilet paper. You're welcome.",
+    },
+]
