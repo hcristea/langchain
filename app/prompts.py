bd0f881a6c396a4487a8f37c9b8677e7a5b578a2
Horatiu Cristea <horatiu.cristea@gmail.com>
2024-04-15 18:24:52 +0300

    Refactor for better narative

--- app/prompts.py
+++ app/prompts.py
@@ -1,10 +1,11 @@
 import typer
 from dotenv import load_dotenv
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 from langchain_core.prompts import FewShotPromptTemplate, PromptTemplate
-from langchain_openai import ChatOpenAI
-from rich.progress import Progress, SpinnerColumn, TextColumn
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-from utils import BUNDY_WISDOM, invoke_w_spinner, loading_message
+from utils import BUNDY_WISDOM, invoke_w_spinner
 
 load_dotenv()
 
@@ -25,7 +26,7 @@ Answer: """
 
 
 @app.command()
-def template():
+def variables():
     llm = ChatOpenAI(model="gpt-3.5-turbo")
 
     template = """You are a travel agent that helps with people's trips about {interest} on a budget of {budget}. 
@@ -35,7 +36,8 @@ Question: What would be a good itinerary for spending three days in Romania and 
 Answer: """
 
     prompt_template = PromptTemplate(
-        input_variables=["interest", "budget"], template=template
+        input_variables=["interest", "budget"],
+        template=template,
     )
     prompt = prompt_template.format(interest="fishing", budget="5000 USD")
 
@@ -44,13 +46,12 @@ Answer: """
 
 
 @app.command()
-def examples():
+def two_shots():
     llm = ChatOpenAI(model="gpt-3.5-turbo")
 
     prompt = """The following are excerpts from conversations with an AI
 assistant. The assistant is typically sarcastic and witty, producing
-creative  and funny responses to the users questions. Here are some
-examples: 
+creative and funny responses to the users questions. Here are some examples: 
 
 User: How are you?
 AI: I can't complain but sometimes I still do.
@@ -66,41 +67,55 @@ AI: """
 
 
 @app.command()
-def few_shot():
+def few_shots():
     llm = ChatOpenAI(model="gpt-3.5-turbo")
-    llm.temperature = 0.1
+    llm.temperature = 0.9
 
-    # create a example template
+    # create the example template
     example_template = """
-    User: {question}
-    AI: {answer}
-    """
+User: {question}
+AI: {answer}
+"""
 
-    # create a prompt example from above template
+    # create the prompt example from above template
     example_prompt = PromptTemplate(
-        input_variables=["question", "answer"], template=example_template
+        input_variables=["question", "answer"],
+        template=example_template,
     )
+
+    examples = BUNDY_WISDOM
 
     # now break our previous prompt into a prefix and suffix
     # the prefix is our instructions
-    prefix = """The following are excerpts from conversations with an AI
-assistant. The assistant is typically sarcastic and witty, producing
-creative  and funny responses to the users questions. Here are some
-examples: 
+    prefix = """The following are excerpts from conversations with an AI assistant. 
+The assistant is typically sarcastic and witty, producing creative and funny responses to the users questions. 
+Here are some examples: 
 """
     # and the suffix our user input and output indicator
     suffix = """
 User: {question}
 AI: """
 
+    example_selector = SemanticSimilarityExampleSelector.from_examples(
+        # The list of examples available to select from.
+        examples,
+        # The embedding class used to produce embeddings which are used to measure semantic similarity.
+        OpenAIEmbeddings(),
+        # The VectorStore class that is used to store the embeddings and do a similarity search over.
+        Chroma,
+        # The number of examples to produce.
+        k=1,
+    )
+
     # now create the few shot prompt template
     few_shot_prompt_template = FewShotPromptTemplate(
-        examples=BUNDY_WISDOM,
+        examples=examples,
+        # example_selector=example_selector,
         example_prompt=example_prompt,
         prefix=prefix,
         suffix=suffix,
         input_variables=["question"],
-        example_separator="\n\n",
+        example_separator="\n",
     )
 
     question = "What is the meaning of life?"
